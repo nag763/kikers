1e6394546ee86e4ddd927929fe2a0a8176826ab1
nag763 <loic.labeye@tutanota.de>
2022-03-07 21:07:12 +0100

    Prepare CLI to fetch api

---
+++ ffb-cli/src/error.rs
@@ -0,0 +1,44 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub enum CliError {
+    RedisError(String),
+    VarError(String),
+    RequestError(String),
+}
+
+impl fmt::Display for CliError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", match &*self {
+            CliError::RedisError (reason) => reason,
+            CliError::VarError (reason) => reason,
+            CliError::RequestError (reason) => reason
+        })
+    }
+}
+
+impl std::error::Error for CliError {}
+
+impl From<redis::RedisError> for CliError {
+    
+    fn from(err : redis::RedisError) -> Self {
+        Self::RedisError(err.to_string())
+    }
+
+}
+
+impl From<std::env::VarError> for CliError {
+
+    fn from(err : std::env::VarError) -> Self {
+        Self::VarError(err.to_string())
+    }
+
+}
+
+impl From<reqwest::Error> for CliError {
+
+    fn from(err : reqwest::Error) -> Self {
+        Self::RequestError(err.to_string())
+    }
+
+}
--- ffb-cli/src/main.rs
+++ ffb-cli/src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+pub mod error;
+
+use error::CliError;
+use dotenv::dotenv;
+
+extern crate redis;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+    std::process::exit(match run_main().await {
+        Ok(_) => {
+            println!("Process exited with success");
+            0
+        },
+        Err(err) => {
+            eprintln!("An error happened : {}", err);
+            eprintln!("The application finished with return code 1");
+            1
+        }
+    });
 }
+
+
+async fn run_main() -> Result<(), CliError> {
+    dotenv().ok();
+    let client = redis::Client::open(std::env::var("REDIS_URL")?)?;
+    let mut con = client.get_connection()?;
+    let client = reqwest::Client::builder()
+        .build()?;
+    let res = client
+        .get(std::env::var("API_PROVIDER")? + "/leagues?current=true")
+        .header("x-rapidapi-host", "api-football-v1.p.rapidapi.com")
+        .header("x-rapidapi-key", std::env::var("API_TOKEN")?)
+        .send()
+        .await?
+        .json::<serde_json::Value>()
+        .await?;
+
+    redis::cmd("SET").arg("leagues").arg(res.to_string()).query(&mut con)?;
+    Ok(())
+}
+

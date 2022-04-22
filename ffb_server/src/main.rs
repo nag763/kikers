cb7d718eec64dd85475290a3cd3053848a0d5f0e
nag763 <loic.labeye@tutanota.de>
2022-04-23 01:16:42 +0200

    Add security headers

--- ffb_server/src/main.rs
+++ ffb_server/src/main.rs
@@ -28,6 +28,7 @@ use actix_files as fs;
 use actix_web::middleware::Logger;
 use actix_web::web;
 use actix_web::ResponseError;
+use actix_web::middleware as actix_middleware;
 use actix_web::{App, HttpServer};
 use actix_web_validator::{FormConfig, QueryConfig};
 use dotenv::dotenv;
@@ -42,6 +43,16 @@ async fn main() -> std::io::Result<()> {
         App::new()
             // Logging config
             .wrap(Logger::new("[%a]->'%U'(%s)"))
+            .wrap(actix_middleware::DefaultHeaders::new()
+                  .add(("X-Version", "0.1"))
+                  .add(("Content-Security-Policy", "default-src 'self'; script-src 'nonce-2726c7f26c'; style-src 'self' 'nonce-7a616b6f6b'; img-src 'self' https://media.api-sports.io"))
+                  .add(("Content-Type", "text/html; charset=utf-8"))
+                  .add(("X-Frame-Options", "DENY"))
+                  .add(("Referrer-Policy", "no-referrer"))
+                  .add(("X-Content-Type-Options", "nosniff"))
+                  .add(("Access-Control-Allow-Origin", "null"))
+                  .add(("Permissions-Policy", "accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()"))
+                  )
             // Default service config
             .default_service(web::to(|| async {
                 ApplicationError::NotFound.error_response()

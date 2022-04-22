980f205bbc2201c20d0d6a753a47220cd1b31ddd
nag763 <loic.labeye@tutanota.de>
2022-04-22 21:46:14 +0200

    Add logs for application data

--- ffb_server/src/application_data.rs
+++ ffb_server/src/application_data.rs
@@ -11,11 +11,14 @@ pub struct ApplicationData {
 
 impl ApplicationData {
     pub async fn init() -> Result<ApplicationData, ApplicationError> {
-        Ok(ApplicationData {
+        info!("Begin of init of application data");
+        let application_data : ApplicationData = ApplicationData {
             role_navaccess: navaccess::Entity::get_role_navaccess_mapping().await?,
             jwt_path: std::env::var("JWT_TOKEN_PATH")?,
             cookie_approval_path: std::env::var("COOKIE_APPROVAL_PATH")?,
-        })
+        };
+        info!("Application data initialized with succes :)");
+        Ok(application_data)
     }
 
     pub fn get_navaccess_for_role(&self, role_id: &u32) -> Vec<NavAccess> {

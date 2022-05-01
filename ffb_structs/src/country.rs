8ffe1c3584ab6a28ee3a21aacc696c5c44d6afa9
nag763 <loic.labeye@tutanota.de>
2022-05-01 18:35:10 +0200

    Cache countries

--- ffb_structs/src/country.rs
+++ ffb_structs/src/country.rs
@@ -14,6 +14,14 @@ pub struct Entity;
 
 impl Entity {
     pub async fn find_all() -> Result<Vec<Model>, ApplicationError> {
+        let mut conn = Database::acquire_redis_connection()?;
+        let countries_as_string : Option<String> = redis::cmd("GET").arg("countries")
+            .query(&mut conn)?;
+        if let Some(countries_as_string) = countries_as_string {
+            let models : Vec<Model> = serde_json::from_str(&countries_as_string)?;
+            Ok(models)
+        } else {
+
         let database = Database::acquire_mongo_connection().await?;
         let models: Vec<Model> = database
             .collection::<Model>("country")
@@ -21,10 +29,18 @@ impl Entity {
             .await?
             .try_collect()
             .await?;
+         redis::cmd("SET")   
+                .arg("countries")
+                .arg(serde_json::to_string(&models)?)
+                .arg("EX")
+                .arg("500")
+                .query(&mut conn)?;
         Ok(models)
+        }
     }
 
     pub async fn store(value: &str) -> Result<(), ApplicationError> {
+        let mut conn = Database::acquire_redis_connection()?;
         let database = Database::acquire_mongo_connection().await.unwrap();
         let models: Vec<Model> = serde_json::from_str(value)?;
         database
@@ -35,6 +51,8 @@ impl Entity {
             .collection::<Model>("country")
             .insert_many(models, None)
             .await?;
+        redis::cmd("DEL").arg("countries")
+            .query(&mut conn)?;
         Ok(())
     }
 }

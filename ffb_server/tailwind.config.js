942241657b3484196012abeb11eb317a68e4500c
nag763 <loic.labeye@tutanota.de>
2022-06-02 20:39:58 +0200

    Continue responsive

--- ffb_server/tailwind.config.js
+++ ffb_server/tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ['./templates/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+	gridTemplateRows: {
+		 '12': 'repeat(12, minmax(0, 1fr))'
+	},
+	gridRow: {
+        	'span-11': 'span 11 / span 11',
+      	}
+    },
   },
   plugins: [],
 }

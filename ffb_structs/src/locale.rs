371ed8bdddda97b02d9baee4b2bde31621edb0cb
nag763 <loic.labeye@tutanota.de>
2022-06-07 19:56:53 +0200

    Fix locale order

--- ffb_structs/src/locale.rs
+++ ffb_structs/src/locale.rs
@@ -16,7 +16,7 @@ impl Entity {
 
     pub async fn get_locales() -> Result<Vec<Model>, ApplicationError> {
         let mut conn = Database::acquire_sql_connection().await?;
-        let models : Vec<Model> = sqlx::query_as("SELECT * FROM LOCALE")
+        let models : Vec<Model> = sqlx::query_as("SELECT * FROM LOCALE ORDER BY long_name")
             .fetch_all(&mut conn)
             .await?;
         Ok(models)

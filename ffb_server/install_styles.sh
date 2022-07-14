a490334ea8f39d59a72495885899e80cc3390688
nag763 <loic.labeye@tutanota.de>
2022-07-14 17:40:54 +0200

    Back to v3.16 following tailwind cli's update to 3.16

--- ffb_server/install_styles.sh
+++ ffb_server/install_styles.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-npx tailwindcss@3.1.4 -i input.css -o styles/tailwind.css -c tailwind.config.js --minify
+npx tailwindcss -i input.css -o styles/tailwind.css -c tailwind.config.js --minify
